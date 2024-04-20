da1c811b54e79b0342711ac7f7da71d1b29fc32c
HuaFuHum <535538261@qq.com>
2024-04-20 09:10:00 +0800

    Add project files.

---
+++ BlazorDiagramIllustration/BlazorDiagramIllustration/Components/CustomizedGroup/MyGroup.cs
@@ -0,0 +1,11 @@
+﻿using Blazor.Diagrams.Core.Models;
+
+namespace BlazorDiagramIllustration.Components.CustomizedGroup
+{
+    public class MyGroup : GroupModel
+    {
+        public MyGroup(IEnumerable<NodeModel> children, byte padding = 30, bool autoSize = true) : base(children, padding, autoSize)
+        {
+        }
+    }
+}
